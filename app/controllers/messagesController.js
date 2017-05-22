dc3314d8629b3d2fdd18dc09a05b29087c001d16
1412414 <thienphuvuong@gmail.com>
2017-05-22 13:14:05 +0700

    cập nhật chức năng nhấn nút new message để gửi tin nhắn cho bạn bè

--- app/controllers/messagesController.js
+++ app/controllers/messagesController.js
@@ -1,64 +1,76 @@
 var messages = require('../models/messages');
 
 var messagesController = {
-  index: function(req, res) {
-    if (req.user) {
-      res.render('messages/index', {
-        layout: 'layout',
-        activeLeftmenuMessages: 'active',
-        activeNavbarMessages: 'active',
-        email: req.user.email
-      });
-    }
-    else {
-      res.render('messages/index', {
-        layout: 'layout',
-        activeLeftmenuUsers: 'active',
-        activeNavbarUsers: 'active',
-      });
-    }
-  },
-  sendMessage: function(req, res) {
-    if (req.user) {
-      messages.getAllFriends(req.user.id, function(err, result) {
-        if (err) {
-          res.render('messages/sendMessage', {
-            layout: 'layout',
-            activeLeftmenuMessages: 'active',
-            activeNavbarMessages: 'active',
-            email: req.user.email
-          });
+    index: function(req, res) {
+        if (req.user) {
+            messages.check(req, function(err, result) {
+                res.render('messages/index', {
+                    layout: 'layout',
+                    activeLeftmenuMessages: 'active',
+                    activeNavbarMessages: 'active',
+                    email: req.user.email,
+                    result: result,
+                    isAuthenticated: req.user.id
+                });
+            });
+        } else {
+            res.render('messages/index', {
+                layout: 'layout',
+                activeLeftmenuUsers: 'active',
+                activeNavbarUsers: 'active',
+            });
         }
-        else {
-          res.render('messages/sendMessage', {
-            layout: 'layout',
-            activeLeftmenuMessages: 'active',
-            activeNavbarMessages: 'active',
-            email: req.user.email,
-            result: result
-          });
+    },
+    sendMessage: function(req, res) {
+        if (req.user) {
+            messages.getAllFriends(req.user.id, function(err, result) {
+                if (err) {
+                    res.render('messages/sendMessage', {
+                        layout: 'layout',
+                        activeLeftmenuMessages: 'active',
+                        activeNavbarMessages: 'active',
+                        email: req.user.email
+                    });
+                } else {
+                    res.render('messages/sendMessage', {
+                        layout: 'layout',
+                        activeLeftmenuMessages: 'active',
+                        activeNavbarMessages: 'active',
+                        email: req.user.email,
+                        result: result
+                    });
+                }
+            });
+        } else {
+            res.redirect('/Messages');
         }
-      });
-    }
-    else {
-      res.redirect('/Messages');
-    }
-  },
-  sendNewMessage: function(req, res) {
-    if (req.user) {
-      messages.createNewMessage(req, function(err, res) {
-        if (err) {
-          res.send({success: '', err: 'There has been an error'});
+    },
+    sendNewMessage: function(req, res) {
+        if (req.user) {
+            messages.createNewMessage(req, function(err, result) {
+                if (err) {
+                    res.redirect('/Messages/NewMessage');
+                } else {
+                    messages.getAllFriends(req.user.id, function(err, result2) {
+                        if (err) {
+                            res.redirect('/Messages/NewMessage');
+                        } else {
+                            res.render('messages/sendMessage', {
+                                layout: 'layout',
+                                activeLeftmenuMessages: 'active',
+                                activeNavbarMessages: 'active',
+                                email: req.user.email,
+                                result: result2,
+                                successMessage: 'successfully'
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+            res.redirect('/Messages');
         }
-        else {
-          res.send({success: 'successfully', err: ''});
-        }
-      });
     }
-    else {
-      res.redirect('/Messages');
-    }
-  }
 };
 
 module.exports = messagesController;
--- app/models/messages.js
+++ app/models/messages.js
@@ -2,25 +2,41 @@ var pg = require('pg');
 var pool = require('../lib/db');
 
 var messages = {
-  getAllFriends: function(id, callback) {
-    pool.query('select f.friend, u.name, u.email from users u, friends f where u.iduser = f.friend and f.iduser = $1', [id], function(error, result) {
-      if (error) {
-        callback(error, null);
-      }
+    check: function(req, callback) {
+        pool.query("select * from messages , users  where idUserRec = $1 and messages.idUserSend = users.idUser ", [req.user.id], function(err, result) {
+            if (err) {
+                callback('Connectting to your messages have failed !!', null);
+            } else {
+                callback(null, result.rows);
+            }
+        });
+    },
+    getAllFriends: function(id, callback) {
+        pool.query('select f.friend, u.name, u.email from users u, friends f where u.iduser = f.friend and f.iduser = $1', [id], function(error, result) {
+            if (error) {
+                callback(error, null);
+            }
 
-      callback(error, result.rows);
-    });
-  },
-  sendNewMessage: function(req, callback) {
-    pool.query('insert into messages() values()', [], function(err, result) {
-      if (err) {
-        callback(err, null);
-      }
-      else {
-        callback(null, result);
-      }
-    });
-  }
+            callback(error, result.rows);
+        });
+    },
+    createNewMessage: function(req, callback) {
+      pool.query('select iduser from users where email = $1', [req.body.email], function(err, res) {
+        console.log(req.body.email);
+        if (err) {
+            callback(err, null);
+        }
+        else {
+          pool.query('insert into messages(idUserRec, idUserSend, timesend, contenttext, status) values($1, $2, CURRENT_TIMESTAMP, $3, false)', [res.rows[0].iduser, req.user.id, req.body.content], function(error, result) {
+              if (error) {
+                  callback(error, null);
+              } else {
+                  callback(null, result);
+              }
+          });
+        }
+      });
+    }
 };
 
 module.exports = messages;
